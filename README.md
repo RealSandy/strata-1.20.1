bb92286350508774517f6d60edf94b493943fd32
Reuben <reuben@twoshoes.org.uk>
2023-10-08 14:22:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Strata
+
+A WIP fabric mod for minecraft 1.20.1
+
+5:36 timestamp setup video
---
+++ src/main/java/net/realsandy/strata/StrataClient.java
@@ -0,0 +1,10 @@
+package net.realsandy.strata;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StrataClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
